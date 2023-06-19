278781086a1b9b31bf180ce1a3debb094412b3aa
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2023-06-19 14:40:31 -0400

    ci: config main .ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+import helmet from 'helmet';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { AllExceptionFilter } from './common/exception-filters/all-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new AllExceptionFilter(configService));
+  app.setGlobalPrefix('', { exclude: ['health'] });
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+  app.enableCors();
+  await app.listen(configService.get<string>('port'));
 }
 bootstrap();
