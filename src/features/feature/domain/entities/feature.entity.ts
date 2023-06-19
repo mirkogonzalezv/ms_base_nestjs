d47bf1a0ae4ba75ce713275d9de55fb01c01c2d3
Mirko Gonzalez <mirko.gonzalez@xintec.cl>
2023-06-19 15:24:25 -0400

    ci: generando modelo de datos con entidad relacionada

---
+++ src/features/feature/domain/entities/feature.entity.ts
@@ -0,0 +1,27 @@
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { IFeatureModel } from '../models/feature.model';
+import { IsBoolean, IsEmail, IsString } from 'class-validator';
+
+export class FeatureEntity implements IFeatureModel {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @IsString()
+  username: string;
+
+  @Column()
+  @IsEmail()
+  email: string;
+
+  @Column({ nullable: true })
+  password: string;
+
+  @Column()
+  @IsBoolean()
+  isActive: boolean;
+
+  constructor() {
+    this.isActive = false;
+  }
+}
---
+++ src/features/feature/domain/models/feature.model.ts
@@ -0,0 +1,6 @@
+export interface IFeatureModel {
+  username: string;
+  email: string;
+  password: string;
+  isActive: boolean;
+}
